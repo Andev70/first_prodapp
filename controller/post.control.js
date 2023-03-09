d7461fdb20794e4e83ce8c173113e3cb81a18d1e
andev70 <anupalnath70@gmail.com>
2023-03-09 09:53:21 +0530

    added a test controller

--- controller/post.control.js
+++ controller/post.control.js
@@ -4,18 +4,21 @@ const fs = require("fs");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-const addPost = async (req, res) => {
+const createPost = async (req, res) => {
   try {
     console.log(req.body);
-    console.log(req.headers);
-    const posts = await Post.create(req.body);
-    if (!posts) return res.status(401).json({ msg: "cannot create post" });
-
-    res.status(201).json({ msg: ["post created successfully", posts] });
+    const auth_ID = req.headers.authorization;
+    if (!auth_ID) return res.status(401).json({ msg: "un authorized user" });
+    const jwt_token = jwt.verify(auth_ID, process.env.JWT_SECRET);
+    const user_ID = jwt_token.id;
+    req.body.userID = user_ID;
+    const post = await Post.create(req.body);
+    if (!post) return res.status(500).json({ msg: "cannot post" });
+    res.status(201).json({ msg: "posted", post });
   } catch (e) {
     console.log(e);
     res.status(500).json({ msg: "error occured" });
   }
 };
 
-module.exports = { addPost };
+module.exports = { createPost };
