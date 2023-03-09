8803565b5ec719536d5a2f1d498b3bd09e5488e7
andev70 <anupalnath70@gmail.com>
2023-03-09 09:55:42 +0530

    front end change to test post

--- public/add-post/main.js
+++ public/add-post/main.js
@@ -1,32 +1,31 @@
-const email = document.querySelector(".email");
-const name = document.querySelector(".name");
-const id = document.querySelector(".id");
-const btn = document.querySelector(".btn");
-console.log(email);
-console.log(btn);
-const posts = async () => {
-  try {
-    const res = await fetch("/api/v1/posts", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: name.value,
-        userID: id.value,
-        user_pic: email.value,
-      }),
-    });
-    const data = res.json();
-    return data;
-  } catch (e) {
-    console.log("error");
-  }
+const form = document.querySelector(".form");
+const token = sessionStorage.getItem("token");
+const sendForm = async () => {
+  const email = document.querySelector(".email");
+  const name = document.querySelector(".name");
+  const id = document.querySelector(".id");
+  const btn = document.querySelector(".btn");
+  const files = document.querySelector(".file").files;
+  const formData = new FormData(form);
+
+  Object.keys(files).forEach((key) => {
+    formData.append("dp", files.item(key));
+  });
+
+  const res = await fetch("/api/v1/posts", {
+    method: "POST",
+    headers: {
+      authorization: token,
+      Accept: "application/json",
+      token: "jjjj",
+    },
+    body: formData,
+  });
+  const data = res.json();
+  return data;
 };
 
-btn.addEventListener("click", (e) => {
-  console.log(email.value, name.value);
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  posts().then((res) => console.log(res));
+  sendForm().then((res) => console.log(res));
 });
