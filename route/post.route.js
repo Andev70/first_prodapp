0fddde57ab7947b9754502105bc5f10a6d754a83
andev70 <anupalnath70@gmail.com>
2023-03-10 16:33:01 +0530

    small bug fix

--- route/post.route.js
+++ route/post.route.js
@@ -3,14 +3,15 @@ const expressFile = require("express-fileupload");
 const router = express.Router();
 const path = require("path");
 const { getComments, createComment } = require("../controller/posts-comment");
-const { createPoste } = require("../controller/post.control");
+const { createPost } = require("../controller/post.control");
 
 router.route("/").post(
   expressFile({
     debug: true,
     useTempFiles: true,
     tempFileDir: path.join(__dirname, "../temp"),
-  })
+  }),
+  createPost
 );
 router.route("/comment").get(getComments).post(createComment);
 module.exports = router;
