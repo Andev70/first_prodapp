d3f3d61326aec37c0e6de099e155be0702c027c0
andev70 <anupalnath70@gmail.com>
2023-03-10 16:09:30 +0530

    router for adding comment is added

--- route/post.route.js
+++ route/post.route.js
@@ -2,16 +2,15 @@ const express = require("express");
 const expressFile = require("express-fileupload");
 const router = express.Router();
 const path = require("path");
-const { getComments } = require("../controller/posts-comment");
-const { createPost } = require("../controller/post.control");
+const { getComments, createComment } = require("../controller/posts-comment");
+const { createPoste } = require("../controller/post.control");
 
 router.route("/").post(
   expressFile({
     debug: true,
     useTempFiles: true,
     tempFileDir: path.join(__dirname, "../temp"),
-  }),
-  createPost
+  })
 );
-router.route("/comment").get(getComments);
+router.route("/comment").get(getComments).post(createComment);
 module.exports = router;
